276e65273e4fade3f61b622a66e3ab4df0b04a6f
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-05-23 20:15:30 -0400

    Updates

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        //Include Unsplash API key here after Client-ID.
-        Authorization: 'Client-ID '
-    }
-});
